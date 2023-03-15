6b35d1688ac517519c21e0aa591b97362c77e9b2
poojakhandale <poojakhandale2000@gmail.com>
2023-03-15 22:44:02 +0530

    EnhancedInvoice

--- Invoicegenerator.cs
+++ Invoicegenerator.cs
@@ -6,30 +6,30 @@ using System.Threading.Tasks;
 
 namespace CabInvoiceGenerator
 {
-   
 
-        public class Invoicegenerator
+
+    public class InvoiceGenerator
+    {
+
+        public double CostPerKm = 10;
+        public int CostPerMinute = 1;
+        public int MinimumFare = 5;
+        public int count;
+        public double CalculateFare(double Distance, int Time)
         {
-            public double CostPerKm = 10;
-            public int CostPerMinute = 1;
-            public int MinimumFare = 5;
-            public int count;
-            public double CalculateFare(double Distance, int Time)
+            count++;
+            double Fare = (CostPerKm * Distance) + (CostPerMinute * Time);
+            return Math.Max(Fare, MinimumFare);
+        }
+        public double CalculateFare(MultipleRide[] ride)
+        {
+            double totalfare = 0;
+            foreach (MultipleRide rides in ride)
             {
-                count++;
-                double Fare = (CostPerKm * Distance) + (CostPerMinute * Time);
-                return Math.Max(Fare, MinimumFare);
+                totalfare = totalfare + CalculateFare(rides.Distance, rides.Time);
             }
-            public double CalculateFare(MultipleRide[] ride)
-            {
-                double totalfare = 0;
-                foreach (MultipleRide rides in ride)
-                {
-                    totalfare = totalfare + CalculateFare(rides.Distance, rides.Time);
-                }
-                return totalfare;
-            }
+            return totalfare;
         }
     }
-
+}
 
