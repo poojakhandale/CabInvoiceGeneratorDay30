ebf7b8cfb4d3577a049fa93e3409eff8013a472b
poojakhandale <poojakhandale2000@gmail.com>
2023-03-15 22:19:36 +0530

    program for multiple Rides

--- Program.cs
+++ Program.cs
@@ -1,15 +1,36 @@
 ﻿using CabInvoiceGenerator;
 
-namespace CabinInvoiceGenerator
-{
-    public class Program
+
+    namespace CabinInvoiceGenerator
     {
-        public static void Main(string[] args)
+        public class Program
         {
-            Console.WriteLine("Welcome to Cab Invoice Generator!");
-            Invoicegenerator uc1 = new();
-            double fare1 = uc1.CalculateFare(10, 30);
-            Console.WriteLine("Total Fare for the Journey is :" + fare1);
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Cab Invoice Generator!");
+
+                while (true)
+                {
+                    Console.WriteLine("Please choose the option :\n1)Calculate Fare\n2)Aggregate Amount for Multiple Rides");
+                    int option = Convert.ToInt16(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                        Invoicegenerator uc1 = new();
+                            double fare1 = uc1.CalculateFare(10, 30);
+                            Console.WriteLine("Total Fare the your Journey is :" + fare1);
+                            break;
+                        case 2:
+                          Invoicegenerator uc2 = new();
+                            MultipleRide[] rides = { new MultipleRide(10, 20), new MultipleRide(15, 25) };
+                            double totalfare = uc2.CalculateFare(rides);
+                            Console.WriteLine("Aggregate value for Multiple rides is :" + totalfare);
+                            break;
+                        default:
+                            Console.WriteLine("Please choose the correct option!");
+                            break;
+                    }
+                }
+            }
         }
     }
-}
