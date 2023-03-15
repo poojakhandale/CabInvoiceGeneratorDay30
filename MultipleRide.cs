030f3a73563d34911fc9a51b771f1151163c3a38
poojakhandale <poojakhandale2000@gmail.com>
2023-03-15 22:22:59 +0530

    multipleRides

---
+++ MultipleRide.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class MultipleRide
+    {
+        public double Distance;
+        public int Time;
+        public MultipleRide(double Distance, int Time)
+        {
+            this.Time = Time;
+            this.Distance = Distance;
+        }
+    }
+}
+
+    
+
