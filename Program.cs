3503ef401f89e6842d23c78f527d077217a3faf5
poojakhandale <poojakhandale2000@gmail.com>
2023-03-15 21:48:20 +0530

    Calculate fare

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Welcome to Cab Invoice Generator!");
+﻿using CabInvoiceGenerator;
 
+namespace CabinInvoiceGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator!");
+            Invoicegenerator uc1 = new();
+            double fare1 = uc1.CalculateFare(10, 30);
+            Console.WriteLine("Total Fare for the Journey is :" + fare1);
+        }
     }
-}
 }
