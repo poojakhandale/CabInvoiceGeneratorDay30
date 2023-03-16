3c4a501595e726b6ee6a26037fba87247b0dcb3c
poojakhandale <poojakhandale2000@gmail.com>
2023-03-16 18:33:31 +0530

    invoice with userid

--- Invoicegenerator.cs
+++ Invoicegenerator.cs
@@ -4,10 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CabInvoiceGenerator
+namespace CabinInvoiceGenerator
 {
-
-
     public class InvoiceGenerator
     {
 
@@ -15,21 +13,32 @@ namespace CabInvoiceGenerator
         public int CostPerMinute = 1;
         public int MinimumFare = 5;
         public int count;
+
         public double CalculateFare(double Distance, int Time)
         {
             count++;
             double Fare = (CostPerKm * Distance) + (CostPerMinute * Time);
             return Math.Max(Fare, MinimumFare);
         }
-        public double CalculateFare(MultipleRide[] ride)
+        public double CalculateFare(MultipleRides[] ride)
         {
             double totalfare = 0;
-            foreach (MultipleRide rides in ride)
+            foreach (MultipleRides rides in ride)
             {
                 totalfare = totalfare + CalculateFare(rides.Distance, rides.Time);
+
             }
             return totalfare;
         }
+        public void InvoiceSummary(MultipleRides[] ride)
+        {
+            foreach (MultipleRides rides in ride)
+            {
+
+                Console.WriteLine("Distance Travelled :" + rides.Distance);
+                Console.WriteLine("Time Taken :" + rides.Time);
+                Console.WriteLine("Fare for ride is :" + CalculateFare(rides.Distance, rides.Time));
+            }
+        }
     }
 }
-
