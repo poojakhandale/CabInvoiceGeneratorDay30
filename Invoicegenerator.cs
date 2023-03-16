b160cb7134d46998f713d4096157a119cee0640d
poojakhandale <poojakhandale2000@gmail.com>
2023-03-16 20:40:12 +0530

    Noramal And Premium Fare

--- Invoicegenerator.cs
+++ Invoicegenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,12 +17,25 @@ namespace CabinInvoiceGenerator
 
         public double CalculateFare(double Distance, int Time)
         {
+
             count++;
             double Fare = (CostPerKm * Distance) + (CostPerMinute * Time);
             return Math.Max(Fare, MinimumFare);
         }
-        public double CalculateFare(MultipleRides[] ride)
+        public double CalculateFare(MultipleRides[] ride, string RideType)
         {
+            if (RideType == "Normal")
+            {
+                CostPerKm = 10;
+                CostPerMinute = 1;
+                MinimumFare = 5;
+            }
+            else if (RideType == "Premium")
+            {
+                CostPerKm = 15;
+                CostPerMinute = 2;
+                MinimumFare = 20;
+            }
             double totalfare = 0;
             foreach (MultipleRides rides in ride)
             {
@@ -30,11 +44,12 @@ namespace CabinInvoiceGenerator
             }
             return totalfare;
         }
-        public void InvoiceSummary(MultipleRides[] ride)
+        public void InvoiceSummary(MultipleRides[] ride, string RideType)
         {
+
             foreach (MultipleRides rides in ride)
             {
-
+                Console.WriteLine("Ride Type :" + RideType);
                 Console.WriteLine("Distance Travelled :" + rides.Distance);
                 Console.WriteLine("Time Taken :" + rides.Time);
                 Console.WriteLine("Fare for ride is :" + CalculateFare(rides.Distance, rides.Time));
